5b74d2302cdc4f5c8ff80c87daa76c49921f55a3
gonzalococ2015 <gonzaloemanuel39@gmail.com>
2023-09-23 13:19:01 -0300

    copia

--- src/main/java/com/proyecto/sistemapenitenciario/servlets/usuario/SvUsuariosModificar.java
+++ src/main/java/com/proyecto/sistemapenitenciario/servlets/usuario/SvUsuariosModificar.java
@@ -25,7 +25,7 @@ public class SvUsuariosModificar extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html;charset=UTF-8");//
         
     }
     
