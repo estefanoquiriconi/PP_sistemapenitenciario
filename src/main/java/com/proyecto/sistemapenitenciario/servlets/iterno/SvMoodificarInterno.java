143a6165342041274cb45025e0a3f36400b792a0
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2023-10-04 22:50:17 -0300

    Estadisticas de establecimientos OK - Cantidad de internos por establecimiento

--- src/main/java/com/proyecto/sistemapenitenciario/servlets/iterno/SvMoodificarInterno.java
+++ src/main/java/com/proyecto/sistemapenitenciario/servlets/iterno/SvMoodificarInterno.java
@@ -17,15 +17,7 @@ import javax.servlet.http.HttpSession;
 public class SvMoodificarInterno extends HttpServlet {
 ControladoraInterno control= new ControladoraInterno();
         ControladoraEstablecimiento controlEstablecimientos = new ControladoraEstablecimiento();
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+        
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
